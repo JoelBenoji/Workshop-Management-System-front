7bf697ffa522555f9686bb752a560766bcfa1ef0
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-06-18 01:26:40 +0530

    Homepage Hero and Staff Login

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,24 @@
 import './styles/home.css'
-import { motion } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import video from './Videos/homevideo.mp4'
 import logo from './Images/output.png'
+import { useRef } from 'react'
 
 export default function Home(){
+
+
+    const targetRef = useRef(null);
+    const {scrollYProgress}= useScroll({
+        target: targetRef,
+        offset: ["end end", "end start"]
+    })
+    const opacityHero = useTransform(scrollYProgress, [0,0.25], [1,0])
+    const opacityDown = useTransform(scrollYProgress, [0,0.001], [1,0])
+
+
     return(
         <div className='homepage'>
-            <div className="video">
+        <div className="video">
         <video src={video} autoPlay loop muted/>
         </div>
         <motion.nav
@@ -15,37 +27,35 @@ export default function Home(){
         >
             <ul>
                 <li className='logo'><img src={logo} alt='logo'/></li>
-                <li><a href='#home' className='link'>Home</a></li>
+                <li><a href='/' className='link'>Home</a></li>
                 <li><a href='#about' className='link'>Who we are</a></li>
                 <li><a href='#services' className='link'>What we do</a></li>
                 <li><a href='#book' className='link'>Book Now</a></li>
                 <li><a href='#contact' className='link'>Contact</a></li>
-                <li><a href='/login' className='link account'>Account</a></li>
+                <li><a href='/user/login' className='link account'>Account</a></li>
             </ul>
         </motion.nav>
         <div className="content">
-        <section className='container container-first home' id='home'>
+        <motion.section style={{opacity:opacityHero}} ref={targetRef} className='container-first' id='home'>
             <motion.h1 animate={{y:-20}} initial={{y:20}} transition={{ease: "easeOut", duration:1}}>Welcome to mec<span>X</span></motion.h1>
-            <p>Scroll down to learn more</p>
-        </section>
+            <motion.p style={{opacity: opacityDown}}>Scroll down to learn more</motion.p>
+        </motion.section>
         <section className='container about bg' id='about'>
-            <h1>Who we are</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia accusamus sed veniam, repellendus quo temporibus! Quis debitis dolore consequuntur repudiandae! Perferendis illo 
-            tempore tenetur quis nisi optio accusantium quia adipisci neque vitae aperiam, ducimus at dolorem dolores, error harum laborum recusandae hic quisquam suscipit
-             doloribus dolor vel praesentium? Est, maxime.
+            <h2>Who we are</h2>
+            At mecX, we're changing the way you service your car. With our convenient online platform, you can schedule appointments, track progress, and access top-quality car services from anywhere. Our certified technicians and transparent pricing ensure a hassle-free experience. Join mecX today and discover the future of car servicing.
         </section>
         <section className='container services bg' id='services'>
-            <h1>What we do</h1>
+            <h2>What we do</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia accusamus sed veniam, repellendus quo temporibus! Quis debitis dolore consequuntur repudiandae! Perferendis illo 
             tempore tenetur quis nisi optio accusantium quia adipisci neque vitae aperiam, ducimus at dolorem dolores, error harum laborum recusandae hic quisquam suscipit
              doloribus dolor vel praesentium? Est, maxime.
         </section>
         <section className='container book bg' id='book'>
-            <h1>Book Now</h1>
+            <h2>Book Now</h2>
             <p>Want to book an Appointment with us?. <br></br>Click the button below</p>
         </section>
         <section className='container contact bg' id='contact'>
-            <h1>Contact Us</h1>
+            <h2>Contact Us</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia accusamus sed veniam, repellendus quo temporibus! Quis debitis dolore consequuntur repudiandae! Perferendis illo 
             tempore tenetur quis nisi optio accusantium quia adipisci neque vitae aperiam, ducimus at dolorem dolores, error harum laborum recusandae hic quisquam suscipit
              doloribus dolor vel praesentium? Est, maxime.
